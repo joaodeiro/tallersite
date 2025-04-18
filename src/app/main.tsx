9260d1a7e0fd1d6309a27e51ad70eaa8fb9712a5
João Deiró <joao.deiro@taller.net.br>
2025-04-17 21:08:40 -0300

    fix: adiciona index.html e main.tsx

---
+++ src/app/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '../styles/globals.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div>
+      <h1>Taller Site</h1>
+    </div>
+  </React.StrictMode>,
+) 
